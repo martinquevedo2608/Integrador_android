2dfd13e168e8e9b63f608e763e22763d84da26b2
martinquevedo2608 <martinquevedo2608@gmail.com>
2025-02-02 01:06:01 -0300

    splash screen anticuado

--- app/src/main/java/com/example/a2025a1_integrador2/SplashScreenActivity.kt
+++ app/src/main/java/com/example/a2025a1_integrador2/SplashScreenActivity.kt
@@ -1,10 +1,14 @@
 package com.example.a2025a1_integrador2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.os.Looper
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,11 @@ class SplashScreenActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Espera 2 segundos y luego abre MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Cierra la SplashActivity
+        }, 2000)
     }
 }
